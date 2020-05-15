b7635d1772baac7799cf0eedbfd65c1f798f121f
Pierre-Henri Trancart <PHT@MacBook-Pro-de-Pierre-Henri.local>
2020-05-15 12:11:15 +0200

    dernier commit avt deploiement

--- routes/home-route.js
+++ routes/home-route.js
@@ -20,6 +20,7 @@ const public_key = process.env.MARVEL_PUBLIC_KEY;
 const hash = md5(ts + private_key + public_key);
 // console.log(hash);
 
+//route homepage avec les characters
 router.get("/", async (req, res) => {
   const response = await axios.get(
     "http://gateway.marvel.com/v1/public/characters",
@@ -35,6 +36,29 @@ router.get("/", async (req, res) => {
 
   res.json(response.data);
   console.log(response.data.data);
+});
+
+//route affichant un seul character
+router.get("/character/:id", async (req, res) => {
+  console.log(req.params);
+  const characterId = req.params;
+  try {
+    const response = await axios.get(
+      `http://gateway.marvel.com/v1/public/characters/${characterId}/comics`,
+      {
+        params: {
+          apikey: public_key,
+          ts: ts,
+          hash: hash,
+        },
+      }
+    );
+
+    res.json(response);
+    console.log(response);
+  } catch (err) {
+    return res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
